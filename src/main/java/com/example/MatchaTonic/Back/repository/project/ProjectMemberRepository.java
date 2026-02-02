d2611df79f8ca194f1e6d5a1e9bbf3cf27c9d907
seoyeon <2371013@ewhain.net>
2026-02-02 17:22:29 +0900

    feat: 특정 프로젝트에 속한 멤버 조회, 참여중 목록조회 (#5)

--- src/main/java/com/example/MatchaTonic/Back/repository/project/ProjectMemberRepository.java
+++ src/main/java/com/example/MatchaTonic/Back/repository/project/ProjectMemberRepository.java
@@ -4,8 +4,15 @@ import com.example.MatchaTonic.Back.entity.project.Project;
 import com.example.MatchaTonic.Back.entity.project.ProjectMember;
 import com.example.MatchaTonic.Back.entity.login.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 import java.util.Optional;
 
 public interface ProjectMemberRepository extends JpaRepository<ProjectMember, Long> {
+    // 특정 유저가 속한 모든 프로젝트 멤버십 조회 (HOME-01)
+    List<ProjectMember> findByUser(User user);
+
+    // 특정 프로젝트에 속한 모든 멤버 조회 (HOME-04)
+    List<ProjectMember> findByProject(Project project);
+
     Optional<ProjectMember> findByUserAndProject(User user, Project project);
 }
